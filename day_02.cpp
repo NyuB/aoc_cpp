8f3eb42fa7e0df97ed1adda77c4e24d66862e542
Brice Decaestecker <brice.decaestecker@gmx.fr>
2024-12-03 21:49:29 +0100

    Day 02 Part Two

--- day_02.cpp
+++ day_02.cpp
@@ -8,16 +8,25 @@
 #include <string>
 #include <vector>
 
-bool consistent(std::vector<unsigned int> const &numbers) {
+using skip_at = std::vector<unsigned int>::const_iterator;
+
+bool consistent(std::vector<unsigned int> const &numbers, skip_at skipped) {
   int slope = 0;
   if (numbers.size() < 2)
     return false;
   for (auto it = numbers.begin(); it < numbers.end() - 1; it++) {
-    if (*it > *(it + 1)) {
+    if (it == skipped)
+      continue;
+    auto next = it + 1;
+    if (next == skipped)
+      next++;
+    if (next == numbers.end())
+      break;
+    if (*it > *next) {
       if (slope > 0)
         return false;
       slope = -1;
-    } else if (*it < *(it + 1)) {
+    } else if (*it < *next) {
       if (slope < 0)
         return false;
       slope = 1;
@@ -28,9 +37,16 @@ bool consistent(std::vector<unsigned int> const &numbers) {
   return true;
 }
 
-bool slow(std::vector<unsigned int> const &numbers) {
+bool slow(std::vector<unsigned int> const &numbers, skip_at skipped) {
   for (auto it = numbers.begin(); it < numbers.end() - 1; it++) {
-    unsigned int diff = std::max(*it, *(it + 1)) - std::min(*it, *(it + 1));
+    if (it == skipped)
+      continue;
+    auto next = it + 1;
+    if (next == skipped)
+      next++;
+    if (next == numbers.end())
+      break;
+    unsigned int diff = std::max(*it, *next) - std::min(*it, *next);
     if (diff > 3)
       return false;
     if (diff < 1)
@@ -40,17 +56,30 @@ bool slow(std::vector<unsigned int> const &numbers) {
 }
 
 bool safe(std::vector<unsigned int> const &numbers) {
-  return slow(numbers) && consistent(numbers);
+  return slow(numbers, numbers.end()) && consistent(numbers, numbers.end());
+}
+
+bool almost_safe(std::vector<unsigned int> const &numbers) {
+  for (auto skipped = numbers.begin(); skipped != numbers.end(); skipped++) {
+    if (slow(numbers, skipped) && consistent(numbers, skipped))
+      return true;
+  }
+  return false;
 }
 
 unsigned int solve_part_one(std::vector<std::string> const &lines) {
   return std::count_if(lines.begin(), lines.end(), [](auto line) {
     const auto is = ints(line);
-    return slow(is) && consistent(is);
+    return safe(is);
   });
 }
 
-unsigned int solve_part_two(std::vector<std::string> const &lines) { return 0; }
+unsigned int solve_part_two(std::vector<std::string> const &lines) {
+  return std::count_if(lines.begin(), lines.end(), [](auto line) {
+    const auto is = ints(line);
+    return almost_safe(is);
+  });
+}
 
 #ifdef DOCTEST_CONFIG_DISABLE
 int main(int argc, char *argv[]) {
@@ -78,4 +107,16 @@ TEST_CASE("Example Part One") {
                "1 3 6 7 9",
            }),
            2);
+}
+
+TEST_CASE("Example Part Two") {
+  CHECK_EQ(solve_part_two({
+               "7 6 4 2 1",
+               "1 2 7 8 9",
+               "9 7 6 2 1",
+               "1 3 2 4 5",
+               "8 6 4 4 1",
+               "1 3 6 7 9",
+           }),
+           4);
 }
